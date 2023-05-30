a5132afaaabfd4c172ab9cebe6f6e4d8dc68cccd
Tomas Jais <tomijais@gmail.com>
2023-05-30 17:35:15 -0300

    feat: update propiedad with new src dir

--- src/app/propiedades/[id]/page.tsx
+++ src/app/propiedades/[id]/page.tsx
@@ -4,23 +4,25 @@ import Image from "next/image";
 
 // TODO set state y use effect
 export default async function Propiedad({ params }: { params: any }) {
-  const { id, images, title } = deptos[params.id - 1];
+  const { id, images, title, imageDir } = deptos[params.id - 1];
   
 
-  const response = await fetch(`/api/imageHandler?id=${params.id - 1}`);
-  const imageFiles = await response.json()
+  // const response = await fetch(`/api/imageHandler?id=${params.id - 1}`);
+  // const imageFiles = await response.json()
 
   return (
     <>
       <h1>Bienvenido al {title}</h1>
 
       <li key={id}>
-        {imageFiles.map((fileName: any ) => {
+        {images.map((fileName: any ) => {
+          console.log(`/${imageDir}/${fileName}`);
+          
           return (
             <Image
               key={fileName}
               alt={fileName}
-              src={`/${images}/${fileName}`}
+              src={`/${imageDir}/${fileName}`}
               width={100}
               height={100}
             />
