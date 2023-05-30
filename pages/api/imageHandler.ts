25ca6723ea8ca62e4f10ce0654e1b9405ada07e3
Tomas Jais <tomijais@gmail.com>
2023-05-30 17:36:13 -0300

    fix: comment all imageHandler api

--- pages/api/imageHandler.ts
+++ pages/api/imageHandler.ts
@@ -1,21 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
-import path from "path";
-import departamentos from "../../src/departamentos.json";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import fs from "fs";
+// import path from "path";
+// import departamentos from "../../src/departamentos.json";
 
-// Fake users data
-const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// // Fake users data
+// const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
-export default function GET(_req: NextApiRequest, res: NextApiResponse) {
-  // Get data from your database
+// export default function GET(_req: NextApiRequest, res: NextApiResponse) {
+//   // Get data from your database
 
-  const { id } = _req.query;
+//   const { id } = _req.query;
 
-  const { images } = departamentos[Number(id)];
+//   const { images } = departamentos[Number(id)];
 
-  const imageDirectory = path.join(process.cwd(), `public/${images}`);
+//   const imageDirectory = path.join(process.cwd(), `public/${images}`);
 
-  const imageFiles = fs.readdirSync(imageDirectory);
+//   const imageFiles = fs.readdirSync(imageDirectory);
 
-  return res.status(200).json(imageFiles);
-}
+//   return res.status(200).json(imageFiles);
+// }
