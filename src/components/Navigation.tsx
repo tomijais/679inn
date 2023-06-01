8bbf55d937fb42defcfb8a4a10e30ba603546bf1
Tomas Jais <tomijais@gmail.com>
2023-06-01 20:28:23 -0300

    style: reorder files

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import NavigationButtons from "./NavigationButtons";
+import NavigationButtons from "./buttons/NavigationButtons";
 const links = [
   {
     label: "Inicio",
