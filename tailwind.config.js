80005d4f30e24cc6844189c5cb8b3b94526e8bd1
Tomas Jais <tomijais@gmail.com>
2023-06-01 23:24:27 -0300

    feat: create new bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "hero-pattern": "url('/hero1.jpeg')",
-        "hero2-pattern": "url('/hero2.jpeg')",
+        "hero-custom-pattern": "url('/hero3.png')",
       },
     },
   },
