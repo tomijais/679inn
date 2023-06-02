50f1f400955312ff1e3128db0c5462708683d065
Tomas Jais <tomijais@gmail.com>
2023-06-01 23:48:09 -0300

    feat: comment accommodationInfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import AccommodationInfo from "@/components/AccommodationInfo";
-import Hero from "../components/Hero";
+// import AccommodationInfo from "@/components/AccommodationInfo";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <AccommodationInfo/>
+      {/* <AccommodationInfo/> */}
     </>
   );
 }
