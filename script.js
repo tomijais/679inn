8e2707c3466c5b384d35f3534d36f33c953c27c8
Tomas Jais <tomijais@gmail.com>
2023-06-04 23:51:55 -0300

    feat: update script

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const fs = require('fs')
 const path = require('path')
 const departamentos = require('./src/departamentos.json')
 
-  const mainDir = path.join(process.cwd(), `public/deptos`);
+const mainDir = path.join(process.cwd(), `public/deptos`);
 
 
-  const imageDir = fs.readdirSync(mainDir);
+const imageDir = fs.readdirSync(mainDir);
 
-    for (let i = 0; i < imageDir.length; i++) {
-        const e = fs.readdirSync(`${mainDir}/${imageDir[i]}`);
-        console.log(e);
-        departamentos[i].images = e;
-    }
+for (let i = 0; i < imageDir.length; i++) {
+  const e = fs.readdirSync(`${mainDir}/${imageDir[i]}`);
+  console.log(e);
+  departamentos[i].images = e;
+  departamentos[i].price = `$${Math.floor(Math.random() * 10000).toLocaleString("de-DE")}`;
+  departamentos[i].price2 = `$${Math.floor(Math.random() * 10000).toLocaleString("de-DE")}`;
+  departamentos[i].price3 = `$${Math.floor(Math.random() * 10000).toLocaleString("de-DE")}`;
+
+}
 
 
-    console.log(departamentos);
+console.log(departamentos);
 
 
-    fs.writeFileSync('./src/departamentos.json', JSON.stringify(departamentos))
+fs.writeFileSync('./src/departamentos.json', JSON.stringify(departamentos))
